02c1e8697908d7847409fee5c9db22c264fccab5
eci4ever <eci4ever@gmail.com>
2026-09-06 11:16:37 +0800

    Point siteRepo to eci4ever/cf-blog

--- src/consts.ts
+++ src/consts.ts
@@ -6,7 +6,7 @@
 export const SITE_METADATA = {
   theme: "system", // Options: system, light, dark
   siteUrl: "https://tasb.yon.fun/",
-  siteRepo: "https://github.com/wanoo21/tailwind-astro-starting-blog",
+  siteRepo: "https://github.com/eci4ever/cf-blog",
   robots: "index, follow", // Options: index, noindex, follow, nofollow
 
   // These are not supported yet
